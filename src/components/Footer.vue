69ca3e016267187fbf8330d6e9522cbaa820fe77
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-07 20:41:33 +0100

    added footer component and formatted navbar file

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>MeetMe</strong> by Luc Joosten - 23INVT2A - 2140212
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footer"
+};
+</script>
+
+<style scoped>
+</style>
