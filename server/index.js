776ac5beb61b524b720c9a8f6e5cdfc7fab9bf18
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-07 21:44:35 +0100

    added mongodb atlas connection string and logger function

--- server/index.js
+++ server/index.js
@@ -1,36 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const express = require("express");
+const mongoose = require("mongoose"); // driver for mongoDB
+const bodyParser = require("body-parser");
+const config = require("./config/dev"); // config variables
+const logger = config.logger;
 
+// Models
 require("./models/meetups");
 require("./models/users");
 require("./models/threads");
 require("./models/posts");
 require("./models/categories");
 
-const meetupsRoutes = require('./routes/meetups'),
-      usersRoutes = require('./routes/users'),
-      threadsRoutes = require('./routes/threads'),
-      postsRoutes = require('./routes/posts'),
-      categoriesRoutes = require('./routes/categories');
+// Routes
+const meetupsRoutes = require("./routes/meetups"),
+  usersRoutes = require("./routes/users"),
+  threadsRoutes = require("./routes/threads"),
+  postsRoutes = require("./routes/posts"),
+  categoriesRoutes = require("./routes/categories");
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true })
-  .then(() => console.log('DB Connected!'))
-  .catch(err => console.log(err));
+// Database connection
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => logger.info("DB Connected!"))
+  .catch(err => logger.error(err));
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/meetups', meetupsRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/posts', postsRoutes);
-app.use('/api/threads', threadsRoutes);
-app.use('/api/categories', categoriesRoutes);
+// Back-end routes for crud operations
+app.use("/api/meetups", meetupsRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/threads", threadsRoutes);
+app.use("/api/categories", categoriesRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , function() {
-  console.log('App is running on port: ' + PORT);
+// Determine on wich port the server is listening to
+app.listen(PORT, function() {
+  logger.debug("App is running on port: " + PORT + " in " + process.env.NODE_ENV + " mode");
 });
