f62603bd1a0918ced480ad9cb32b36eb2eb37bd3
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-07 20:52:18 +0100

    added files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+  
 import Vue from 'vue'
 import App from './App.vue'
+import AppDropdown from './components/AppDropdown'
+import AppHero from './components/AppHero'
 
 Vue.config.productionTip = false
+
+Vue.component('AppHero', AppHero)
+Vue.component('AppDropdown', AppDropdown)
 
 new Vue({
   render: h => h(App),
