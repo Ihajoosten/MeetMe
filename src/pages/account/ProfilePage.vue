0834a39e067b7d94533f75118af82f24ffbca872
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-21 22:10:01 +0100

    updated styling

--- src/pages/account/ProfilePage.vue
+++ src/pages/account/ProfilePage.vue
@@ -1,53 +1,59 @@
 <template>
   <div class="container profile">
     <div class="row">
-      <div class="col-5">
-          <img class="profile-pic" :src="user.avatar" />
-
-        <br />
-        <br />
-
-        <span class="title"
-          ><h4>
-            <b>{{ user.name }}</b>
-          </h4></span
-        >
-        <p class="tagline">Email: {{ user.email }}</p>
-        <p class="tagline">Username: {{ user.username }}</p>
-        <p class="tagline">Joined meetings: {{user.joinedMeetings.length}}</p>
-        <button class="btn btn-sm btn-outline-primary mt-2 mb-4">Update Info</button>
-
-
-        
+      <div class="col-sm text-center">
+        <img class="profile-pic" :src="user.avatar" />
+          <span class="title"
+            ><h4>
+              <b>{{ user.name }}</b>
+            </h4></span
+          >
+          <p class="tagline">Username: {{ user.username }}</p>
+          <p class="tagline">
+            Joined meetings: {{ user.joinedMeetings.length }}
+          </p>
+          <button class="btn btn-sm btn-outline-primary">
+            Update Info
+          </button>
       </div>
+        <div class="stats-tab col-sm text-center">
+          <p class="stat-val">{{ meetingCount }}</p>
+          <p class="stat-key">Meetups</p>
+        </div>
 
-      <div class="stats-tab col-2 text-center">
-        <p class="stat-val">{{ meetingCount }}</p>
-        <p class="stat-key">Meetups</p>
-      </div>
+        <div class="stats-tab col-sm text-center">
+          <p class="stat-val">{{ threadCount }}</p>
+          <p class="stat-key">Threads</p>
+        </div>
 
-      <div class="stats-tab col-2 text-center">
-        <p class="stat-val">{{ threadCount }}</p>
-        <p class="stat-key">Threads</p>
-      </div>
-
-      <div class="stats-tab col-2 text-center">
-        <p class="stat-val">{{ postCount }}</p>
-        <p class="stat-key">Posts</p>
-      </div>
+        <div class="stats-tab col-sm text-center">
+          <p class="stat-val">{{ postCount }}</p>
+          <p class="stat-key">Posts</p>
+        </div>
     </div>
 
-    <div class="row mt-2">
-      <div v-for="meeting in meetings" v-bind:key="meeting._id" class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img :src="meeting.image" style="width: 100%; height: 10rem;" class="card-img-top" alt="..." />
+    <div class="row mt-5">
+      <div
+        v-for="meeting in meetings"
+        v-bind:key="meeting._id"
+        class="col"
+      >
+        <div class="card" style="width: auto;">
+          <img
+            :src="meeting.image"
+            style="width: 100%; height: 10rem;"
+            class="card-img-top"
+            alt="..."
+          />
           <div class="card-body">
-            <h5 class="card-title">
-              {{meeting.title}}
-            </h5>
-              <div class="badge badge-success">{{meeting.status | capitalize}}</div>
+            <h6 class="card-title">
+              {{ meeting.title }}
+            </h6>
+            <div class="badge badge-success">
+              {{ meeting.status | capitalize }}
+            </div>
             <p class="card-text mt-2">
-              {{meeting.description}}
+              {{ meeting.description }}
             </p>
           </div>
           <div class="card-footer">
@@ -61,10 +67,12 @@
 
     <div class="row">
       <div v-for="thread in threads" v-bind:key="thread._id" class="col-md-4">
-        <div class="card" style="width: 18rem; height: 10rem">
+        <div class="card" style="width: auto; height: 10rem">
           <div class="card-body">
-            <h5 class="card-title">{{thread.title}}</h5>
-            <div>Posted <b>{{thread.createdAt | fromNow}}</b></div>
+            <h5 class="card-title">{{ thread.title }}</h5>
+            <div>
+              Posted <b>{{ thread.createdAt | fromNow }}</b>
+            </div>
           </div>
           <div class="card-footer">
             <button class="btn btn-sm btn-outline-success mr-2">Share</button>
@@ -77,10 +85,12 @@
 
     <div class="row">
       <div v-for="post in posts" v-bind:key="post._id" class="col-md-4">
-        <div class="card" style="width: 18rem; height: 10rem">
+        <div class="card" style="width: auto; height: 10rem">
           <div class="card-body">
-            <h5 class="card-title">{{post.text}}</h5>
-            <div>Posted <b>{{post.createdAt | fromNow}}</b></div>
+            <h5 class="card-title">{{ post.text }}</h5>
+            <div>
+              Posted <b>{{ post.createdAt | fromNow }}</b>
+            </div>
           </div>
           <div class="card-footer">
             <button class="btn btn-sm btn-outline-success mr-2">Share</button>
@@ -120,16 +130,21 @@ body {
   background: #f5f7fa;
 }
 .profile-pic {
-    height: 75px;
-    width: 75px;
+  height: 100px;
+  width: 100px;
 }
 .stats-tab {
+  border-left: 1px solid transparent;
   border-bottom: 2px solid transparent;
+  border-right: 1px solid transparent;
+
   transition: 0.5s;
 }
 .stats-tab:hover {
   cursor: pointer;
+  border-left: 1px solid #28a745;
   border-bottom: 2px solid #28a745;
+  border-right: 1px solid #28a745;
 }
 .stats-tab.isActive {
   border-bottom: 2px solid #28a745;
@@ -148,13 +163,7 @@ body {
   + .has-text-centered {
   border-left: 1px dotted rgba(6, 253, 6, 0.2);
 }
-.container.profile {
-  margin-top: 1%;
-}
-.control.is-pulled-left span.select {
-  margin-right: 5px;
-  border-radius: 2px;
-}
+
 .modal-card .content h1 {
   padding: 40px 10px 10px;
   border-bottom: 1px solid #dadada;
