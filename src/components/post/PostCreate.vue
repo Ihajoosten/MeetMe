72a003dddb546f036d6fb12358816a2f7cd6ebd2
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-27 23:59:16 +0100

    Update PostCreate.vue

--- src/components/post/PostCreate.vue
+++ src/components/post/PostCreate.vue
@@ -30,8 +30,8 @@ export default {
     };
   },
   computed: {
-    meeeting() {
-      return this.meeting;
+    meeting() {
+      return this.$store.state.meetings.item;
     }
   },
   methods: {
