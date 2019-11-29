ff8bfd7777b812a1387b1faa6748acd380640c7e
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-29 01:33:34 +0100

    changing last controllerfunctions to async

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -3,53 +3,81 @@
 const Category = require('../models/categories');
 
 module.exports = {
-  getAllCategories(req, res, next) {
-    Category.find({}).exec((errors, categories) => {
-      if (errors) {
-        return res.status(422).send({ errors });
-      }
-
-      if (!categories) {
-        return res
-          .status(404)
-          .json({ Message: 'No categories found! Create one' });
-      }
-
-      return res.json(categories);
-    });
-  },
-  createCategories(req, res, next) {
-    const body = req.body;
-
-    let category = new Category({
-      name: body.name,
-      image: body.image
-    });
-
-    category.save((err, user) => {
-      if (err) return res.status(422).json({ err });
-      return res.status(201).json(category);
-    });
-  },
-  updateCategoryById(req, res, next) {
-    const id = req.params.id;
-    const body = req.body;
-    Category.findOneAndUpdate({ _id: id }, body)
-      .then(() => Category.find({ _id, id }))
-      .then(category => {
-        res.status(200).json({ result: category });
+  getAllCategories: async (req, res, next) => {
+    try {
+      await Category.find().exec((errors, categories) => {
+        if (errors) return res.status(422).json({ errors });
+        if (!categories)
+          return res
+            .status(404)
+            .json({ Message: 'No categories found! Create one' });
+        return res.status(200).json(categories);
       });
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
   },
-  deleteCategoryById(req, res, next) {
-    const id = req.params.id;
-    let c;
-    Category.findOne({ _id: id })
-      .then(category => {
-        c = category;
-      })
-      .then(() => Category.findOneAndDelete({ _id: id }))
-      .then(() => {
-        res.status(200).json({ result: c });
+  createCategories: async (req, res, next) => {
+    const body = await req.body;
+
+    if (meeting === null)
+      return res.status(400).json({ message: 'Bad request - empty body' });
+
+    const category = new Category(body);
+    try {
+      await category.save((err, user) => {
+        if (err) return res.status(422).json({ err });
+        return res.status(201).json(category);
       });
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
+  },
+  updateCategoryById: async (req, res, next) => {
+    const { id } = await req.params;
+    const body = await req.body;
+
+    if (body === null)
+      return res.status(400).json({ message: 'Bad request - empty body' });
+    if (id === null)
+      return res
+        .status(404)
+        .json({ message: 'No category specified or does not exist' });
+
+    try {
+      await Category.findByIdAndUpdate(
+        { _id: id },
+        { $set: body },
+        { new: true },
+        (err, result) => {
+          if (err) return res.status(422).json({ err });
+          else return res.status(200).json(result);
+        }
+      );
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
+  },
+  deleteCategoryById: async (req, res, next) => {
+    const { id } = await req.params;
+
+    if (id === null)
+      return res
+        .status(404)
+        .json({ message: 'No meeting specified or does not exist' });
+    try {
+      await Category.findById({ _id: id }, async (err, category) => {
+        if (err) return res.status(422).send({ err });
+        await category.remove((errors, category) => {
+          if (errors) return res.status(422).send({ errors });
+          else
+            return res
+              .status(200)
+              .json({ message: 'Deleted category with id: ' + category._id });
+        });
+      });
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
   }
 };
--- server/controllers/location.js
+++ server/controllers/location.js
@@ -2,15 +2,15 @@ const geoip = require('geoip-lite');
 // Run 'npm run-script updatedb' to update the data file
 const request = require('request');
 
-exports.getMeta = function(req, res) {
+exports.getMeta = async (req, res) => {
   if (process.env.NODE_ENV === 'production') {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const ip = await req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const geo = geoip.lookup(ip);
     return res.json(geo);
   } else {
-    request('https://api.ipify.org?format=json', function (error, response) {
+    request('https://api.ipify.org?format=json', async (error, response) => {
       if (!error && response.statusCode == 200) {
-          const ip = JSON.parse(response.body).ip;
+          const ip = await JSON.parse(response.body).ip;
           const geo = geoip.lookup(ip);
           return res.status(200).json(geo);
       } else {
