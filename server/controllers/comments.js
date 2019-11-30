851a2b11c2b4a4407174ad906a8f4db04d07976b
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-30 20:51:27 +0100

    added new routes and controller functions for comment to update and delete

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -29,30 +29,62 @@ module.exports = {
     } catch (error) {
       if (error) return res.status(500).json({ error });
     }
+  },
+  updateCommentById: async (req, res) => {
+    const { id } = await req.params;
+    const body = await req.body;
+
+    if (body === null)
+      return res.status(400).json({ message: 'Bad request - empty body' });
+    if (id === null)
+      return res
+        .status(401)
+        .json({ message: 'No comment specified or does not exist' });
+
+    try {
+      await Comment.findByIdAndUpdate(
+        id,
+        { $set: body },
+        { new: true },
+        (err, comment) => {
+          if (err) return res.status(422).send({ err });
+          else return res.status(200).json(comment);
+        }
+      );
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
+  },
+  deleteCommentById: async (req, res) => {
+    const { id } = await req.params;
+    const user = await req.user;
+
+    if (id === null)
+      return res.status(400).json({
+        message: 'Bad request - not specified comment or does not exist'
+      });
+    if (user === null)
+      return res.status(401).json({ message: 'Not Authorized' });
+
+    try {
+      await Comment.findById(id, async (err, comment) => {
+        if (err) return res.status(422).send({ err });
+
+        if (comment.author != user._id)
+          return res
+            .status(401)
+            .send({ errors: { message: 'Not Authorized!' } });
+
+        await comment.remove((err, post) => {
+          if (err) return res.status(422).send({ err });
+          return res
+            .status(200)
+            .json({ message: 'Deleted comment with id: ' + post._id });
+        });
+      });
+    } catch (error) {
+      if (error) return res.status(500).json({ error });
+    }
   }
-  //   ,
-
-  //   updateCommentById: (req, res, next) => {
-  //     const id = req.params.id;
-  //     const body = req.body;
-
-  //     Comment.findOneAndUpdate({ _id: id }, body)
-  //       .then(() => Comment.findOne({ _id: id }))
-  //       .then(comment => {
-  //         res.status(200).json({ result: comment });
-  //       });
-  //   },
-
-  //   deleteCommentById: (req, res, next) => {
-  //     const id = req.params.id;
-  //     let c;
-  //     Comment.findOne({ _id: id })
-  //       .then(comment => {
-  //         c = comment;
-  //       })
-  //       .then(() => Comment.findOneAndDelete({ _id: id }))
-  //       .then(() => {
-  //         res.status(200).json({ result: c });
-  //       });
-  //   }
+  
 };
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,6 +4,6 @@ const auth = require('../services/authentication');
 module.exports = app => {
   // Comment endpoints
   app.post("/api/comments", auth.validateToken,  controller.postComment);
-  // app.put("/api/comments/:id", auth.validateToken,  controller.updateCommentById);
-  // app.delete("/api/comments/:id", auth.validateToken,  controller.deleteCommentById);
+  app.patch("/api/comments/:id", auth.validateToken,  controller.updateCommentById);
+  app.delete("/api/comments/:id", auth.validateToken,  controller.deleteCommentById);
 };
