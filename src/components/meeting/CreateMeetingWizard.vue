46222363a4c73d5490704f33bdab83925d46c09a
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-30 21:21:59 +0100

    Update CreateMeetingWizard.vue

--- src/components/meeting/CreateMeetingWizard.vue
+++ src/components/meeting/CreateMeetingWizard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="meeting-create-form">
     <div class="container badge badge-success mt-3">{{ currentPage }} of {{ stepSize }}</div>
-]    <keep-alive>
+   <keep-alive>
       <component
         :is="formSteps[currentPage - 1]"
         v-on:nextStep="mergeData"
