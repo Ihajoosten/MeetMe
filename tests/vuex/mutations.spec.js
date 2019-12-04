0a61edfe0a69c34826aadbee2e6b8db5fb64b43b
Luc Joosten <lhajoost@avans.nl>
2019-12-04 04:58:49 +0100

    added some more mutation tests

--- tests/vuex/mutations.spec.js
+++ tests/vuex/mutations.spec.js
@@ -2,7 +2,7 @@
 import mutations from './store/mutations';
 import { expect } from 'chai';
 
-describe('Testing mutations', () => {
+describe('Testing Meeting mutations', () => {
   it('SetItems', () => {
     const state = {
       items: []
@@ -21,5 +21,118 @@ describe('Testing mutations', () => {
     mutations.setItem(state, { item });
 
     expect(state.item).to.eq(item);
+    expect(typeof state.item).to.eq('object');
+    expect(typeof state.item).not.to.eq('array');
+  });
+
+  it('SetMeeting', () => {
+    const state = {
+      meeting: {}
+    };
+    const meeting = {};
+    mutations.setMeeting(state, { meeting });
+
+    expect(state.item).to.eq(meeting);
+    expect(typeof state.item).to.eq('object');
+    expect(typeof state.item).not.to.eq('array');
+  });
+
+  it('addUserToMeeting', () => {
+    const state = {
+      item: {
+        joinedPeople: []
+      }
+    };
+    const person = {};
+    mutations.addUsertoMeeting(state, person);
+
+    expect(state.item.joinedPeople).to.contain(person);
+    expect(typeof state.item).to.eq('object');
+    expect(typeof state.item).not.to.eq('array');
   });
 });
+
+describe('Testing Thread mutations', () => {
+  it('SavePostToThread', () => {
+    const state = {
+      items: [
+        {
+          posts: []
+        }
+      ]
+    };
+    const post1 = { id: 1 };
+    const post2 = { id: 2 };
+
+    mutations.savePostToThread(state, { threadPosts: post1, index: 0 });
+    expect(state.items[0].posts).contains(post1);
+
+    mutations.savePostToThread(state, { threadPosts: post2, index: 0 });
+    expect(state.items[0].posts).contains(post2);
+
+    expect(typeof state.items[0]).to.eq('object');
+    expect(typeof state.items[0].posts).to.eq('object');
+  });
+
+  it('SaveCommentToPost', () => {
+    const state = {
+      items: [
+        {
+          posts: []
+        }
+      ]
+    };
+    const post1 = {};
+    const comment = {};
+
+    state.items[0].posts.push(post1);
+
+    mutations.saveCommentToPost(state, {
+      postComments: comment,
+      tIndex: 0,
+      pIndex: 0
+    });
+    expect(state.items[0].posts[0].comments).to.contain(comment);
+
+    expect(typeof state.items[0]).to.eq('object');
+    expect(typeof state.items[0].posts).to.eq('object');
+  });
+
+  it('IsAllDataLoaded', () => {
+    const state = {
+      isAllDataLoaded: false
+    }
+
+    const isAllDataLoaded = false;
+
+    mutations.setAllDataLoaded({state, isAllDataLoaded})
+    expect(typeof state.isAllDataLoaded).to.eq('boolean');
+    expect(state.isAllDataLoaded).to.not.equal(true)
+
+    mutations.setAllDataLoaded({state, isAllDataLoaded})
+    expect(typeof state.isAllDataLoaded).to.eq('boolean');
+    expect(state.isAllDataLoaded).to.equal(isAllDataLoaded)
+  });
+
+  it('DeleteThread', () => {
+    const state = {
+      items: []
+    }
+
+    const thread1 = {id: 1};
+    const thread2 = {id: 2};
+
+    state.items.push(thread1);
+    state.items.push(thread2);
+
+    const index = state.items.findIndex(item => item.id === thread1.id)
+
+    mutations.deleteThread(state, index);
+    expect(state.items[0]).to.contain(thread2)
+    mutations.deleteThread(state, index);
+    expect(state.items[0]).to.eq(undefined)
+    expect(state.items[1]).to.eq(undefined)
+  });
+});
+
+describe('Testing Stats mutations', () => {});
--- tests/vuex/store/mutations.js
+++ tests/vuex/store/mutations.js
@@ -1,10 +1,45 @@
 // mutations.js
+import Vue from 'vue';
 
 export default {
+  /** MEETINGS **/
   setItems(state, { items }) {
     state.items = items;
   },
   setItem(state, { item }) {
     state.item = item;
+  },
+  setMeeting(state, { meeting }) {
+    state.item = meeting;
+  },
+  addUsertoMeeting(state, joinedPeople) {
+    Vue.set(state.item, 'joinedPeople', joinedPeople);
+  },
+
+  /** THREADS **/
+  savePostToThread(state, { threadPosts, index }) {
+    Vue.set(state.items[index], 'posts', threadPosts);
+  },
+  saveCommentToPost(state, { postComments, tIndex, pIndex }) {
+    Vue.set(state.items[tIndex].posts[pIndex], 'comments', postComments);
+  },
+  setAllDataLoaded({ state, isAllDataLoaded }) {
+    Vue.bind(state.isAllDataLoaded, isAllDataLoaded);
+  },
+  deleteThread(state, index) {
+    Vue.set(state.items.splice(index, 1));
+  },
+  deletePost(state, { threadIndex, postIndex }) {
+    Vue.set(state.items[threadIndex].posts.splice(postIndex, 1));
+  },
+
+  /** STATS **/
+  setStats(state, stats) {
+    return Object.assign(state, {}, stats);
+  },
+  deleteResource(state, { resource, itemId }) {
+    const index = state[resource].data.findIndex(item => item._id === itemId);
+    state[resource].data.splice(index, 1);
+    state[resource].count--;
   }
 };
